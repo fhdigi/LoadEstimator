c944fec86f92282c17bf2a6fc22c140cb75b76fc
Freddie Heidrich <fredheidrich@lccsny.com>
2017-07-13 12:25:57 -0400

    Lol Look at this Screen thingy i made

---
+++ VilterLoadCalculator/Models/BlastFreezingLoadEstimates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace VilterLoadCalculator.Models
+{
+    class BlastFreezingLoadEstimates
+    {
+        public string Product { get; set; }
+        public int Amount { get; set; }
+        public int Weight { get; set; }
+        public int SupplyAirTemp { get; set; }
+        public int SupplyAirVelocity { get; set; }
+        public int ProductTempEntering { get; set; }
+        public int ProductTempLeaving { get; set; }
+        public int HeatRemoved { get; set; }
+        public string EstimatedTimeHeatRemoval { get; set; }
+
+
+
+
+
+
+    }
+}
